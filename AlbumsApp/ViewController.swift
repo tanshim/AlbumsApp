5f5d588ee749bc3ce01bae9352056fcceb57a7de
Sultan <serioz.jak@gmail.com>
2023-07-01 00:52:46 +0600

    Initial Commit

---
+++ AlbumsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumsApp
+//
+//  Created by Sultan on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
